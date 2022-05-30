1ec601f124cc2c032c8401cfa1f32d9474780323
vipnua <giangvipbg2002@gmail.com>
2022-05-30 18:23:44 +0700

    update

--- ontap/src/pages/detail.js
+++ ontap/src/pages/detail.js
@@ -5,8 +5,9 @@ import data from '@/data';
 const ProductDetail = {
     render(id) {
         // Tìm id trong mảng data
-        const product = data.find((item) => item.id === id);
-        // nếu không có thì return về string not found
+        // const product = data.find((item) => item.id === id);
+        const product = await (await fetch(`http://localhost:3001/products/${id}`))
+            // nếu không có thì return về string not found
         if (!product) return `<div>Not found</div>`;
         // ngược lại nếu có trả về dữ liệu
 
