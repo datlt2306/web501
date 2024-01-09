ef8169957358b3c00a4aa254fbfe3d89a319a722
Le Trong Dat <datlt2306@gmail.com>
2024-01-09 14:53:09 +0700

    Buổi 4

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+const root = document.getElementById("root");
+const myName = "Dat";
+const myAge = 20;
+const isMarried = true;
+const myInfo = {
+    name: "Kien",
+    age: 30,
+};
+ReactDOM.createRoot(root).render(
+    <>
+        <h1>Hello {myName}</h1>
+        <p>Age: {myAge}</p>
+        <p>Status: {isMarried ? "Đã chết và chưa chôn" : "Pede"} </p>
+        <p>
+            Info: {myInfo.name} - {myInfo.age}
+        </p>
+        <p>lorem ipsum</p>
+    </>
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// const myName = "Dat";
+// const text = `<h1>hello ${myName}</h1>`;
+// const root = document.querySelector("#root");
+
+// root.innerHTML = text;
