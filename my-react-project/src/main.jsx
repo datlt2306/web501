502ce09a67187c0c89699ed027d1b233350387c8
Le Trong Dat <datlt2306@gmail.com>
2024-01-10 15:39:32 +0700

    Buổi 4

--- my-react-project/src/main.jsx
+++ my-react-project/src/main.jsx
@@ -1,26 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import App from "./App";
 
-const myName = "John";
-const myAge = 20;
-const isMarried = true;
-const myInfo = {
-    name: "John",
-    age: 20,
-    isMarried: true,
-};
 // jsx - javascript xml
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <div className="container">
-        <h1>Tên tôi là : {myName}</h1>
-        <h2>Tuổi: {myAge + 10}</h2>
-        <p>Tình trạng: {isMarried ? "Đã chết" : "3D"}</p>
-        <p>
-            Info: {myInfo.name} - {myInfo.age} - {myInfo.isMarried}
-        </p>
-    </div>
-);
-
-// npm create vite@latest my-react-project -- --template react
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
