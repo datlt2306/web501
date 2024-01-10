25eaa70491f019497ad430a428ae9f8eb96af84a
Le Trong Dat <datlt2306@gmail.com>
2024-01-10 13:53:12 +0700

    Buổi 4

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -2,7 +2,8 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
-// rafce
+// React es7+... : gõ câu lệnh rafce để tạo component
+// cài đặt extension: html to jsx
 function App() {
     return (
         <div>
