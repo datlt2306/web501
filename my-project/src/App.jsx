c77a70f8fa96b66cda42508be1e1d98577005952
Le Trong Dat <datlt2306@gmail.com>
2024-01-11 12:39:37 +0700

    Buổi 5

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./componets/Footer";
+import Header from "./componets/Header";
+import Main from "./componets/Main";
+import ShowName from "./componets/ShowName";
 function App() {
     return (
         <div>
-            <h1>Hello</h1>
+            <ShowName name="John" age={20} />
+            <ShowName name="Dat" age={25} />
+            <Header />
+            <Main />
+            <Footer />
         </div>
     );
 }
--- my-project/src/Demo.js
+++
@@ -1,9 +0,0 @@
-export const a = 10;
-export const b = 20;
-
-export const result = (a, b) => {
-    return a + b;
-};
-
-const moduleDemo = () => {};
-export default moduleDemo;
