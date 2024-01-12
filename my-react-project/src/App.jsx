104bd988cf20aa49f0a5e3a74345df69dff486f3
Le Trong Dat <datlt2306@gmail.com>
2024-01-12 13:06:32 +0700

    Buổi 5

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,12 +1,17 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import ProductItem from "./components/ProductItem";
 
 // React es7+... : gõ câu lệnh rafce để tạo component
 // cài đặt extension: html to jsx
+
 function App() {
     return (
         <div>
+            <ProductItem product={{ name: "Product A", price: 200 }} />
+            <ProductItem product={{ name: "Product B", price: 300 }} />
+            <ProductItem product={{ name: "Product C", price: 400 }} />
             <Header />
             <Main />
             <Footer />
---
+++ my-react-project/src/components/ProductItem.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable react/prop-types */
+function ProductItem(props) {
+    return <h1>{props.product.name}</h1>;
+}
+export default ProductItem;
