e0707ed3019c4a3888ac8d2bf851dc8dbfc4011e
Le Trong Dat <datlt2306@gmail.com>
2024-01-12 14:39:37 +0700

    Buổi 5

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,10 +1,11 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
-
+import ProductItem from "./components/ProductItem";
 function App() {
     return (
         <>
+            <ProductItem product={{ name: "Product A", price: 200 }} />
             <div className="container">
                 <Header />
                 <Main />
---
+++ my-react-project/src/components/ProductItem.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+const ProductItem = (props) => {
+    return <h1>{props.product.name}</h1>;
+};
+
+export default ProductItem;
