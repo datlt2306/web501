1ed398ab6373610f983820ce25c80b082081053a
Le Trong Dat <datlt2306@gmail.com>
2024-01-13 12:45:43 +0700

    Buổi 6

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,18 +2,43 @@ import Footer from "./componets/Footer";
 import Header from "./componets/Header";
 import Main from "./componets/Main";
 import ProductItem from "./componets/ProductItem";
-import ShowName from "./componets/ShowName";
 
-const product = {
-    name: "Iphone 12",
-    price: 1000,
-    color: "red",
-    img: "https://picsum.photos/200/200",
-    address: {
-        city: "HCM",
-        district: "1",
+const productList = [
+    {
+        id: 1,
+        name: "Iphone 12",
+        price: 1000,
+        color: "red",
+        img: "https://picsum.photos/200/200",
+        address: {
+            city: "HCM",
+            district: "1",
+        },
+    }, // product
+    {
+        id: 2,
+        name: "Iphone 13",
+        price: 1000,
+        color: "red",
+        img: "https://picsum.photos/200/200",
+        address: {
+            city: "HCM",
+            district: "1",
+        },
     },
-};
+    {
+        id: 3,
+        name: "Iphone 14",
+        price: 1000,
+        color: "red",
+        img: "https://picsum.photos/200/200",
+        address: {
+            city: "HCM",
+            district: "1",
+        },
+    },
+];
+
 function App() {
     return (
         <div className="max-w-6xl mx-auto">
@@ -23,12 +48,13 @@ function App() {
             <hr />
             <h2>Sản phẩm</h2>
             <div className="grid grid-cols-3 gap-8">
-                <div className="border-2">
-                    <ProductItem product={product} />
-                </div>
-                <div className="border-2">
-                    <ProductItem product={product} />
-                </div>
+                {productList.map(function (product, index) {
+                    return (
+                        <div key={index} className="border-2">
+                            <ProductItem product={product} />
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
