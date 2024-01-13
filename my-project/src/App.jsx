eee2560a5ba4f66901d9b00673868c05abac076f
Le Trong Dat <datlt2306@gmail.com>
2024-01-13 15:02:44 +0700

    Buổi 6

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,22 +1,22 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ProductItem from "./components/ProductItem";
-
-const productList = [
-    {
-        name: "Samsung",
-        price: 200,
-        description: "This is a fruit 1",
-        image: "https://picsum.photos/200/500",
-    }, // item
-    {
-        name: "Samsung",
-        price: 200,
-        description: "This is a fruit 1",
-        image: "https://picsum.photos/200/500",
-    }, // item
-];
 const App = () => {
+    const [productList] = useState([
+        {
+            name: "Samsung",
+            price: 200,
+            description: "This is a fruit 1",
+            image: "https://picsum.photos/200/500",
+        }, // item
+        {
+            name: "Samsung",
+            price: 200,
+            description: "This is a fruit 1",
+            image: "https://picsum.photos/200/500",
+        }, // item
+    ]);
     return (
         <div className="max-w-6xl mx-auto">
             <Header />
