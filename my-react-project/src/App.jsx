32f0fba5bdc0fe3417551db1bbacfdd0418a1162
Le Trong Dat <datlt2306@gmail.com>
2024-01-15 15:36:27 +0700

    Buổi 7

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -9,12 +10,26 @@ const productList = [
     { id: 3, name: "Product C", price: 400 },
 ];
 function App() {
+    const [isLogged, setIsLogged] = useState(false);
+    const [name, setName] = useState("");
+
+    const onChangeStatus = () => {
+        setIsLogged(!isLogged);
+        setName("Lê Trọng Đạt");
+    };
     return (
         <>
+            {isLogged ? (
+                <>
+                    <span>Chào mừng {name}</span>
+                    <button onClick={onChangeStatus}>Đăng xuất</button>
+                </>
+            ) : (
+                <button onClick={onChangeStatus}>Đăng nhập</button>
+            )}
             {productList.map((product) => {
                 return <ProductItem product={product} key={product.id} />;
             })}
-
             <div className="container">
                 <Header />
                 <Main />
