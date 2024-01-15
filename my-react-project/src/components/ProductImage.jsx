cf7713f052bb242c332e419c59f48075373bee9b
Le Trong Dat <datlt2306@gmail.com>
2024-01-15 11:05:01 +0700

    Buổi 7

--- my-react-project/src/components/ProductImage.jsx
+++ my-react-project/src/components/ProductImage.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import React from "react";
 
 const ProductImage = ({ url, size }) => {
     return <img src={url} alt="" width={size} className="w-full" />;
--- my-react-project/src/components/ProductItem.jsx
+++ my-react-project/src/components/ProductItem.jsx
@@ -1,12 +1,9 @@
 /* eslint-disable react/prop-types */
-import React from "react";
 import ProductImage from "./ProductImage";
 
 const ProductItem = ({ product }) => {
     return (
         <div className="shadow rounded">
-            {/* product.image => https://picsum.co/200/200 */}
-            {/* <img src={product.image} alt="" className="w-full" /> */}
             <ProductImage url={product.image} size={20} />
             <h2 className="text-lg">{product.name}</h2>
             <span>{product.price}</span>
