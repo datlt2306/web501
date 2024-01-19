de1cbf5572e3e7a76a25429b9a50722c7b7e0634
Le Trong Dat <datlt2306@gmail.com>
2024-01-19 13:38:49 +0700

    Buổi 8

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,40 +1,64 @@
-import { useState } from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import { useEffect, useState } from "react";
 import ProductItem from "./components/ProductItem";
 
 // React es7+... : gõ câu lệnh rafce để tạo component
 // cài đặt extension: html to jsx
 
 function App() {
-    const [productList] = useState([
-        { name: "Product A", price: 200 }, // product
-        { name: "Product B", price: 300 }, // product
-        { name: "Product C", price: 400 }, // product
-    ]);
+    const [productList, setProductList] = useState([]);
 
-    const [isLogged, setIsLogged] = useState(false);
+    useEffect(() => {
+        const getProducts = async () => {
+            const response = await fetch("https://6110f09bc38a0900171f0ed0.mockapi.io/products");
+            const data = await response.json();
+            setProductList(data);
+        };
+        getProducts();
+    }, []);
+    const productFake = {
+        name: "Product A",
+        img: "https://loremflickr.com/cache/resized/65535_53071549535_dd19303026_c_640_480_nofilter.jpg",
+    };
+    const productFakeUpdate = {
+        name: "Product A update",
+        img: "https://loremflickr.com/cache/resized/65535_53071549535_dd19303026_c_640_480_nofilter.jpg",
+    };
 
-    const changeStatus = () => {
-        setIsLogged(!isLogged);
+    const addProduct = async () => {
+        await fetch("https://6110f09bc38a0900171f0ed0.mockapi.io/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(productFake),
+        });
+    };
+
+    // DELETE
+    const deleteProduct = async () => {
+        await fetch("https://6110f09bc38a0900171f0ed0.mockapi.io/products/23", {
+            method: "DELETE", // PUT - DELETE - GET
+        });
+    };
+
+    // UPDATE
+    const updateProduct = async () => {
+        await fetch("https://6110f09bc38a0900171f0ed0.mockapi.io/products/22", {
+            method: "PUT", // PUT - DELETE - GET
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(productFakeUpdate),
+        });
     };
     return (
         <div>
-            {isLogged ? (
-                <>
-                    <span>Xin chào admin</span>
-                    <button onClick={changeStatus}>Logout</button>
-                </>
-            ) : (
-                <button onClick={changeStatus}>Đăng nhập</button>
-            )}
+            <button onClick={addProduct}>Add</button>
+            <button onClick={deleteProduct}>DELETE</button>
+            <button onClick={updateProduct}>UPDATE</button>
             {productList.map((product) => {
                 return <ProductItem key={product.name} product={product} />;
             })}
-            <Header />
-            <Main />
-            <Footer />
         </div>
     );
 }
--- my-react-project/src/components/ProductItem.jsx
+++ my-react-project/src/components/ProductItem.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 function ProductItem({ product }) {
-    console.log("product", product);
     return <h1>{product.name}</h1>;
 }
 export default ProductItem;
