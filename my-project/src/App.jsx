4314011f9cbf607707b0db1afb58b803f781f7f8
Le Trong Dat <datlt2306@gmail.com>
2024-02-20 12:36:40 +0700

    Validate Form

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,94 +1,34 @@
-import { useState } from "react";
-import Footer from "./componets/Footer";
-import Header from "./componets/Header";
-import Main from "./componets/Main";
-import ProductItem from "./componets/ProductItem";
-
+import { useForm } from "react-hook-form";
 function App() {
-    // state
-    const [productList] = useState([
-        {
-            id: 1,
-            name: "Iphone 12",
-            price: 1000,
-            color: "red",
-            img: "https://picsum.photos/200/200",
-            address: {
-                city: "HCM",
-                district: "1",
-            },
-        }, // product
-        {
-            id: 2,
-            name: "Iphone 13",
-            price: 1000,
-            color: "red",
-            img: "https://picsum.photos/200/200",
-            address: {
-                city: "HCM",
-                district: "1",
-            },
-        },
-        {
-            id: 3,
-            name: "Iphone 14",
-            price: 1000,
-            color: "red",
-            img: "https://picsum.photos/200/200",
-            address: {
-                city: "HCM",
-                district: "1",
-            },
-        },
-    ]);
-    const [isLogged, setIsLogged] = useState(false);
-    const [user, setUser] = useState({});
-
-    // function
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
     return (
-        <div className="max-w-6xl mx-auto">
-            {isLogged ? (
-                <>
-                    <span>Chào mừng bạn {user.name} </span>
-                    <button
-                        className="
-                                bg-green-500 
-                                text-white 
-                                rounded p-4"
-                        onClick={() => {
-                            setIsLogged(!isLogged);
-                            setUser({});
-                        }}
-                    >
-                        Đăng xuất
-                    </button>
-                </>
-            ) : (
-                <button
-                    onClick={() => {
-                        setIsLogged(!isLogged);
-                        setUser({
-                            name: "Lê Trọng Đạt",
-                            email: "datlt34@fpt.edu.vn",
-                        });
-                    }}
-                >
-                    Đăng nhập
-                </button>
-            )}
-            <hr />
-            <Header />
-            <Main />
-            <Footer />
-            <hr />
-            <h2>Sản phẩm</h2>
-            <div className="grid grid-cols-3 gap-8">
-                {productList.map((product, index) => (
-                    <div key={index} className="border-2">
-                        <ProductItem product={product} />
-                    </div>
-                ))}
-            </div>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" {...register("name", { required: true, minLength: 3 })} />
+                {errors.name && errors.name.type == "required" && <span>Bắt buộc phải nhập</span>}
+                {errors.name && errors.name.type == "minLength" && (
+                    <span>Bắt buộc phải nhập ít nhất 3 ký tự</span>
+                )}
+                <input
+                    type="text"
+                    {...register("price", {
+                        required: true,
+                        validate: (value) => !isNaN(value),
+                    })}
+                />
+                {errors.price && errors.price.type == "required" && <span>Bắt buộc phải nhập</span>}
+                {errors.price && errors.price.type == "validate" && (
+                    <span>Bắt buộc phải là số</span>
+                )}
+                <button>Submit</button>
+            </form>
         </div>
     );
 }
